6f3cf1b258245fefbb75716cf8b77207ebfad44e
Mamba1099 <mambakaranja240@gmail.com>
2026-07-20 17:37:44 +0300

    feat(config): add database configuration urls

--- my-app/config/database.php
+++ my-app/config/database.php
@@ -86,12 +86,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravel_db'),
+            'username' => env('DB_USERNAME', 'mamba'),
+            'password' => env('DB_PASSWORD', 'mamba123'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
