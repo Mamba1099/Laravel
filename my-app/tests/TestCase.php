08e7a52d381d00c4c6b1d720221438c8655454e0
Mamba1099 <mambakaranja240@gmail.com>
2026-07-20 17:44:24 +0300

    feat(test): add automatic database refresh for test suite

--- my-app/tests/TestCase.php
+++ my-app/tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
 }
